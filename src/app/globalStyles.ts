44c25aaf2b60f7b100091d2b42074cb285bc4a0b
«Elena <LenaRybinskova@gmail.com>
2025-08-25 13:35:02 +0300

    add fonts

--- src/app/globalStyles.ts
+++ src/app/globalStyles.ts
@@ -1,8 +1,24 @@
 import {createGlobalStyle} from 'styled-components';
-import {colors} from './stylesVar';
+import {colors, fonts} from './stylesVar';
 
 const GlobalStyle = createGlobalStyle`
 
+    @font-face {
+        font-family: 'PT Sans';
+        src: url('/fonts/PTSans-Regular.woff2') format('woff2');
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: 'PT Sans';
+        src: url('/fonts/PTSans-Bold.woff2') format('woff2');
+        font-weight: 700;
+        font-style: normal;
+        font-display: swap;
+    }
+    
   :root {
       --color-primary: #5d5fef;
       --color-secondary: #ef5da8;
@@ -18,6 +34,7 @@ const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
       margin: 0;
       padding: 0;
+      font-family: ${fonts.primary};
   }
   
   body {
@@ -25,6 +42,7 @@ const GlobalStyle = createGlobalStyle`
       margin: 0 auto; 
 /*      width: 100%;*/
       background-color: ${colors.background};
+      
   }
 `;
 
